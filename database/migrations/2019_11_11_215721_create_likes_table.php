eccea81188efcff2741708feeab22b918c0b335e
Rachael Yoder <wayoder4@gmail.com>
2019-11-12 09:21:42 -0500

    relationships between users, posts and likes are set up

--- database/migrations/2019_11_11_215721_create_likes_table.php
+++ database/migrations/2019_11_11_215721_create_likes_table.php
@@ -17,7 +17,7 @@ class CreateLikesTable extends Migration
             $table->bigIncrements('id');
             $table->integer('user_id');
             $table->integer('post_id');
-            $table->integer('likes');
+            $table->boolean('likes');
             $table->timestamps();
         });
     }
